3bfec53a8055e9ee9f8b5f9e0d4d32f2552d6e01
FlashFact17 <d.florindo17@gmail.com>
2020-04-19 21:37:06 -0400

    Main Menu ouline

---
+++ Assets/Menu_Choice.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu_Choice : MonoBehaviour
+{
+    [SerializeField] private GameObject[] panels; //list of all my panels
+    [SerializeField] private Selectable[] defaultButtons; // A list of the button selected by default
+
+
+    public void PanelToggle()
+    {
+        PanelToggle(0); //activate the main menu (position 0)
+    }
+    public void PanelToggle(int position)
+    {
+        //we call this function when we want to open/close a menu.
+        Input.ResetInputAxes();
+        for (int i = 0; i < panels.Length; i++)
+        {
+            panels[i].SetActive(position == i);
+            if (position == i)
+            {
+                defaultButtons[i].Select(); //we select the button from that coresponding panel.
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("PanelToggle", 0.01f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Start_Button.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start_Button : MonoBehaviour
+{
+    private AsyncOperation async; //This is to load a scene in the background
+
+    public void BtnLoadScene()
+    {
+        if (async == null)
+        {
+            Scene currScene = SceneManager.GetActiveScene();
+            async = SceneManager.LoadSceneAsync(currScene.buildIndex + 1);
+            async.allowSceneActivation = true;
+        }
+    }
+
+    public void BtnLoadScene(int i)
+    {
+        if (async == null)
+        {
+            async = SceneManager.LoadSceneAsync(i);
+            async.allowSceneActivation = true;
+        }
+    }
+
+    public void BtnLoadScene(string s)
+    {
+        if (async == null)
+        {
+            async = SceneManager.LoadSceneAsync(s);
+            async.allowSceneActivation = true;
+        }
+    }
+}
